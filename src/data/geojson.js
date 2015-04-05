ab0224627bb575800d74b0660a0457136ea19852
rad <raphael.a.dumas@gmail.com>
2015-04-05 16:29:10 -0400

    SODA working. Not building filters properly though.

--- src/data/geojson.js
+++ src/data/geojson.js
@@ -19,19 +19,7 @@ define(function(require, exports, module) {
 
       
     this.processData = function processData(data) {
-        console.log("Fake Data");
-        var data1 = [
-    { name: 'Location A', category: 'Store', street: 'Market', lat: 39.984, lng: -75.343 },
-    { name: 'Location B', category: 'House', street: 'Broad', lat: 39.284, lng: -75.833 },
-    { name: 'Location C', category: 'Office', street: 'South', lat: 39.123, lng: -74.534 }
-  ];
-
-  console.log (data1);
-    console.log(GeoJSON.parse(data1, {Point: ['lat', 'lng']}));
         
-        
-        console.log("Data");
-        console.log(data);
       for (var i = 0; i < data.length; i++) {
       if (!data[i].id) {
           data[i].id = 'finda-' + i;
@@ -40,17 +28,18 @@ define(function(require, exports, module) {
           delete data[i];    }
 
         else {
-            data[i].lat = parseFloat(data[i].location.latitude);
-            data[i].lng = parseFloat(data[i].location.longitude);
+            data[i].lat = Number(data[i].location.latitude);
+            data[i].lng = Number(data[i].location.longitude);
             delete data[i].location;
         }
       }
       console.log("data processed");
-        console.log(JSON.stringify(data));
-        var data2 = data;
-      data2 = GeoJSON.parse(data2, {Point: ['lat, lng']});
+//        console.log(JSON.stringify(data));
+        
+      var data2 = GeoJSON.parse(data, {Point: ['lat', 'lng']});
+
       console.log("geojson encoded");
-        console.log(JSON.stringify(data2));
+//        console.log(JSON.stringify(data2));
 
       return data2;
     };
